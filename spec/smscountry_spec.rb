f57082421f1ea6590c32f0da918885f79dba868e
Danish Aziz Satkut <donspy@gmail.com>
2023-01-19 22:41:31 +0530

    Remove console logging (#1)
    * Remove response logging to return response.
    
    * Ignore RubyMine .idea dir.
    
    * Upgrade version to v0.1.1

--- spec/smscountry_spec.rb
+++ spec/smscountry_spec.rb
@@ -2,10 +2,6 @@ require 'spec_helper'
 
 describe Smscountry do
   it 'has a version number' do
-    expect(Smscountry::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+    expect(Smscountry::VERSION).to eq('0.1.1')
   end
 end
